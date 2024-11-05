1665cf39642944ce32a55622c1782a5dabc0ecea
kyrolles <kyrollesraafat@gmail.com>
2024-11-05 18:21:22 +0200

    handling the path of the images

--- lib/screens/Invoice_screen.dart
+++ lib/screens/Invoice_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class PaymentScreen extends StatelessWidget {
   const PaymentScreen({super.key});
 
@@ -11,9 +10,7 @@ class PaymentScreen extends StatelessWidget {
         title: const Text('Invoice'),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-          
-          },
+          onPressed: () {},
         ),
       ),
       body: Padding(
@@ -33,31 +30,36 @@ class PaymentScreen extends StatelessWidget {
               child: ListView(
                 children: [
                   statusTile(
-                    imagePath: 'image/daca1c3b78a2c352c89eabda54e640ce.png',
+                    imagePath:
+                        'assets/images/daca1c3b78a2c352c89eabda54e640ce.png',
                     label: 'Proof of enrollment',
                     status: 'Done',
                     statusColor: Colors.green,
                   ),
                   statusTile(
-                    imagePath: 'image/9e1e8dc1064bb7ac5550ad684703fb30.png',
+                    imagePath:
+                        'assets/images/9e1e8dc1064bb7ac5550ad684703fb30.png',
                     label: 'Tuition fees',
                     status: 'Done',
                     statusColor: Colors.green,
                   ),
                   statusTile(
-                    imagePath: 'image/daca1c3b78a2c352c89eabda54e640ce.png',
+                    imagePath:
+                        'assets/images/daca1c3b78a2c352c89eabda54e640ce.png',
                     label: 'Proof of enrollment',
                     status: 'Rejected',
                     statusColor: Colors.orange,
                   ),
                   statusTile(
-                    imagePath: 'image/daca1c3b78a2c352c89eabda54e640ce.png',
+                    imagePath:
+                        'assets/images/daca1c3b78a2c352c89eabda54e640ce.png',
                     label: 'Proof of enrollment',
                     status: 'Pending',
                     statusColor: Colors.yellow,
                   ),
                   statusTile(
-                    imagePath: 'image/daca1c3b78a2c352c89eabda54e640ce.png',
+                    imagePath:
+                        'assets/images/daca1c3b78a2c352c89eabda54e640ce.png',
                     label: 'Proof of enrollment',
                     status: 'No Status',
                     statusColor: Colors.grey,
@@ -66,114 +68,98 @@ class PaymentScreen extends StatelessWidget {
               ),
             ),
             GestureDetector(
-              onTap: () {
-              
-              },
-            
+              onTap: () {},
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Ask for',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 20),
-            GestureDetector(
-              onTap: () {
-              
-              },
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-                
-                const SizedBox(height: 10),
-                 Container(
-                padding: const EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: Row(
-                  children: [
-                
-         Container(
-             decoration: const BoxDecoration(
-             color: Colors.white,  
-             shape: BoxShape.circle, 
-  ),
-                 padding: const EdgeInsets.all(10),  
-                     child: ClipOval(
-                     child: Image.asset(
-                     'image/9e1e8dc1064bb7ac5550ad684703fb30.png',  
-                      width: 60,
-                      height: 60,
-                      fit: BoxFit.cover,  
-    ),
-  ),
-),
-
-                    const SizedBox(width: 15),
-              
-                    const Text(
-                      'Tuition fees',
-                      style: TextStyle(
-                       fontSize: 18,
-                        color: Colors.white,
-                      ),
+                children: [
+                  const Text(
+                    'Ask for',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
                     ),
-                  ],
-                ),
-              ),
-          
-               const SizedBox(height: 10),
-               Container(
-                padding: const EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: Row(
-                  children: [
-                
-         Container(
-             decoration: const BoxDecoration(
-             color: Colors.white,  
-             shape: BoxShape.circle, 
-  ),
-                 padding: const EdgeInsets.all(10),  
-                     child: ClipOval(
-                     child: Image.asset(
-                     'image/daca1c3b78a2c352c89eabda54e640ce.png',  
-                      width: 60,
-                      height: 60,
-                      fit: BoxFit.cover,  
-    ),
-  ),
-),
-
-                    const SizedBox(width: 15),
-              
-                    const Text(
-                      'Proof of enrollment',
-                      style: TextStyle(
-                       fontSize: 18,
-                        color: Colors.white,
-                      ),
+                  ),
+                  const SizedBox(height: 20),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 10),
+                        Container(
+                          padding: const EdgeInsets.all(16.0),
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: Row(
+                            children: [
+                              Container(
+                                decoration: const BoxDecoration(
+                                  color: Colors.white,
+                                  shape: BoxShape.circle,
+                                ),
+                                padding: const EdgeInsets.all(10),
+                                child: ClipOval(
+                                  child: Image.asset(
+                                    'assets/images/9e1e8dc1064bb7ac5550ad684703fb30.png',
+                                    width: 60,
+                                    height: 60,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 15),
+                              const Text(
+                                'Tuition fees',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Container(
+                          padding: const EdgeInsets.all(16.0),
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: Row(
+                            children: [
+                              Container(
+                                decoration: const BoxDecoration(
+                                  color: Colors.white,
+                                  shape: BoxShape.circle,
+                                ),
+                                padding: const EdgeInsets.all(10),
+                                child: ClipOval(
+                                  child: Image.asset(
+                                    'assets/images/daca1c3b78a2c352c89eabda54e640ce.png',
+                                    width: 60,
+                                    height: 60,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 15),
+                              const Text(
+                                'Proof of enrollment',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-          ], 
-            ),
-            ),
-              
-          
-            
-          ],
-           ),
             ),
           ],
         ),
@@ -181,6 +167,7 @@ class PaymentScreen extends StatelessWidget {
     );
   }
 }
+
 Widget statusTile({
   required String imagePath,
   required String label,
@@ -200,7 +187,7 @@ Widget statusTile({
           backgroundColor: Colors.grey[200],
           child: Image.asset(
             imagePath,
-            width: 24, 
+            width: 24,
             height: 24,
           ),
         ),
