d4a335c7b74439aa8f833eb86a97cba6f7f09227
kyrolles <kyrollesraafat@gmail.com>
2025-01-31 12:30:46 +0200

    vallidate screen integrat it with firebase

--- lib/screens/training/staff_training/validate_screen.dart
+++ lib/screens/training/staff_training/validate_screen.dart
@@ -1,120 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:graduation_project/components/kbutton.dart';
 import 'package:graduation_project/components/list_container.dart';
 import 'package:graduation_project/components/my_app_bar.dart';
 import 'package:graduation_project/components/student_container.dart';
+import 'package:graduation_project/constants.dart';
+import 'package:graduation_project/screens/invoice/it_incoive/request_model.dart';
 import 'package:graduation_project/screens/training/staff_training/validate_buttom_sheet.dart';
 
-class ValidateScreen extends StatelessWidget {
-  ValidateScreen({super.key});
+class ValidateScreen extends StatefulWidget {
+  const ValidateScreen({super.key});
 
-  final List<Widget> studentreques = [
-    StudentContainer(
+  @override
+  State<ValidateScreen> createState() => _ValidateScreenState();
+}
+
+class _ValidateScreenState extends State<ValidateScreen> {
+  final Stream<QuerySnapshot> _requestsStream =
+      FirebaseFirestore.instance.collection('requests').snapshots();
+
+  List<Request> requestsList = [];
+
+  List<Widget> get studentreques {
+    return requestsList.map((request) {
+      return StudentContainer(
         onTap: (BuildContext context) {
           showModalBottomSheet(
             backgroundColor: const Color.fromRGBO(250, 250, 250, 0.93),
             context: context,
-            scrollControlDisabledMaxHeightRatio: 0.8,
             builder: (BuildContext context) {
               return const ValidateButtomSheet();
             },
           );
         },
-        name: 'Kyrolles Raafat',
-        status: 'Pending',
-        statusColor: Colors.yellow,
-        id: '20-0-60785',
-        year: '4th',
-        title: 'EGSA.pdf',
-        image: 'assets/project_image/pdf.png'),
-    StudentContainer(
-        onTap: (BuildContext context) {
-          showModalBottomSheet(
-            backgroundColor: const Color.fromRGBO(250, 250, 250, 0.93),
-            context: context,
-            scrollControlDisabledMaxHeightRatio: 0.8,
-            builder: (BuildContext context) {
-              return const ValidateButtomSheet();
-            },
-          );
-        },
-        name: 'Mahmoud Abdelnaserrrrrrrrr',
-        status: 'Pending',
-        statusColor: Colors.yellow,
-        id: '20-0-60785',
-        year: '4th',
-        title: 'EGSA.pdf',
-        image: 'assets/project_image/pdf.png'),
-    StudentContainer(
-        onTap: (BuildContext context) {
-          showModalBottomSheet(
-            backgroundColor: const Color.fromRGBO(250, 250, 250, 0.93),
-            context: context,
-            scrollControlDisabledMaxHeightRatio: 0.8,
-            builder: (BuildContext context) {
-              return const ValidateButtomSheet();
-            },
-          );
-        },
-        name: 'Youssef Abdelfatah',
-        status: 'Done',
-        statusColor: Colors.green,
-        id: '20-0-60785',
-        year: '4th',
-        title: 'EGSA.pdf',
-        image: 'assets/project_image/pdf.png'),
-    StudentContainer(
-        onTap: (BuildContext context) {
-          showModalBottomSheet(
-            backgroundColor: const Color.fromRGBO(250, 250, 250, 0.93),
-            context: context,
-            scrollControlDisabledMaxHeightRatio: 0.8,
-            builder: (BuildContext context) {
-              return const ValidateButtomSheet();
-            },
-          );
-        },
-        name: 'Youssef Abdelfatah',
-        status: 'Rejected',
-        statusColor: Colors.red,
-        id: '20-0-60785',
-        year: '4th',
-        title: 'EGSA.pdf',
-        image: 'assets/project_image/pdf.png'),
-    StudentContainer(
-        onTap: (BuildContext context) {
-          showModalBottomSheet(
-            backgroundColor: const Color.fromRGBO(250, 250, 250, 0.93),
-            context: context,
-            scrollControlDisabledMaxHeightRatio: 0.8,
-            builder: (BuildContext context) {
-              return const ValidateButtomSheet();
-            },
-          );
-        },
-        name: 'Ahmed Tarek',
-        status: 'No status',
-        statusColor: const Color.fromRGBO(229, 229, 229, 1),
-        id: '20-0-60785',
-        year: '4th',
-        title: 'EGSA.pdf',
-        image: 'assets/project_image/pdf.png'),
-    // StudentContainer(
-    //     onTap: null,
-    //     name: null,
-    //     status: null,
-    //     statusColor: null,
-    //     id: null,
-    //     year: null,
-    //     button: (BuildContext context) {
-    //       return const KButton(
-    //         text: 'Download',
-    //         backgroundColor: Colors.blue,
-    //       );
-    //     },
-    //     title: 'EGSA.pdf',
-    //     image: 'assets/project_image/pdf.png'),
-  ];
+        name: request.studentName,
+        status: request.status,
+        statusColor: request.status == 'Pending'
+            ? Colors.yellow
+            : request.status == 'Rejected'
+                ? Colors.red
+                : request.status == 'Done'
+                    ? const Color(0xFF34C759)
+                    : kGreyLight,
+        id: request.studentId,
+        year: request.year,
+        title: request.fileName,
+        image: 'assets/project_image/pdf.png',
+      );
+    }).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -125,14 +59,30 @@ class ValidateScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          ListContainer(
-            title: 'Requests',
-            listOfWidgets: studentreques,
-            emptyMessage: 'No Requests',
+          StreamBuilder<QuerySnapshot>(
+            stream: _requestsStream,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                requestsList = [];
+                for (var i = 0; i < snapshot.data!.docs.length; i++) {
+                  if (snapshot.data!.docs[i]['type'] == 'Training') {
+                    requestsList.add(Request.fromJson(snapshot.data!.docs[i]));
+                  }
+                }
+                return ListContainer(
+                  title: 'Requests',
+                  listOfWidgets: studentreques,
+                  emptyMessage: 'No Requests',
+                );
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            },
           ),
           KButton(
             text: 'Archive',
-            // width: 345,
             height: 62,
             svgPath: 'assets/project_image/Pin.svg',
             onPressed: () {
@@ -140,9 +90,6 @@ class ValidateScreen extends StatelessWidget {
                   context, '/staffStudentTraining/validate/archive');
             },
           ),
-          // const SizedBox(
-          //   height: 10,
-          // )
         ],
       ),
     );
