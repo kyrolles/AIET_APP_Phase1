55f777902cc0316fb7c0bf8eff41ae4af0753647
kyrolles <kyrollesraafat@gmail.com>
2025-06-12 23:21:06 +0300

    change the background color of the card in the archive attendance

--- lib/screens/attendance/it_attendance/it_attendance_archive.dart
+++ lib/screens/attendance/it_attendance/it_attendance_archive.dart
@@ -87,6 +87,7 @@ class ArchivedAttendanceCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.only(bottom: 16),
+      color: Colors.white,
       elevation: 2,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(12),
